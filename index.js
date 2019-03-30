e24471d2d22c661ce57e324011c4e6f6ab66ebac
David Duffy <daviduffy1@gmail.com>
2019-03-30 06:57:14 -0700

    didn't add flattenInputs to exports, oops

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var { FORM_INPUTS_DEFAULT, FORM_ORDER } = require('./src/constants/formInputs.js
 var { EVENT_TYPES_COMMON, EVENT_TYPES_ALL } = require('./src/constants/eventTypes.js');
 var { STATES } = require('./src/constants/states.js');
 var { FORM_STYLES_DEFAULT, FORM_BORDER_PRESETS } = require('./src/constants/styles.js');
+var { flattenInputs } = require('./src/utils/contactForms.js');
 
 module.exports = {
   EVENT_TYPES_ALL,
@@ -12,5 +13,6 @@ module.exports = {
   FORM_BORDER_PRESETS,
   FORM_INPUTS_DEFAULT,
   FORM_ORDER,
-  STATES
+  STATES,
+  flattenInputs
 };
